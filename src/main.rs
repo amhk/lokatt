3f93bc1cad902a1c5ebbc0b7f61e25858562266c
Mårten Kongstad <marten.kongstad@gmail.com>
2019-11-01 16:20:34 +0100

    Basic outline of event loop
    Use a crossbeam channel to send data from multiple threads to the main
    event loop.
    
    Signed-off-by: Mårten Kongstad <marten.kongstad@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use crossbeam_channel::{bounded, Sender};
+use std::io;
+use std::thread;
+
+fn input_thread(s: Sender<u32>) {
+    loop {
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer).unwrap();
+        let i = buffer.trim().parse::<u32>().unwrap();
+        s.send(i).unwrap();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (sender, receiver) = bounded(32);
+
+    let s = sender.clone();
+    thread::Builder::new()
+        .name("input".to_string())
+        .spawn(move || {
+            input_thread(s);
+        })
+        .unwrap();
+
+    drop(sender);
+
+    loop {
+        match receiver.recv().unwrap() {
+            0 => break,
+            x => println!("received {}", x),
+        }
+    }
 }
